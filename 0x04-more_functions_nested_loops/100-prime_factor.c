34b8888517ff011f30a5ca21126478295f144a80
ZubbsZubbs <thankgodazubuike194@gmail.com>
2023-02-26 02:44:01 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,6 +15,7 @@ int main(void)
 		n = 612852475143;
 		max = -1;
 
+
 		while (n % 2 == 0)
 		{
 			max = 2;
@@ -23,11 +24,11 @@ int main(void)
 
 		for (i = 3; i <= sqrt(n); i = i + 2)
 		{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
+			while (n % i == 0)
+			{
+				max = i;
+				n = n / i;
+			}
 		}
 		if (n > 2)
 			max = n;
