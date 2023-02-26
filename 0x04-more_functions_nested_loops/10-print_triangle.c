1a5202fd421dbe80f66b4594f6b716830677d744
ZubbsZubbs <thankgodazubuike194@gmail.com>
2023-02-26 02:04:17 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,32 @@
-#include "main.h"
+#:wq
+include "main.h"
+
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
- */
+ *  * print_triangle - prints a triangle, followed by a new line
+ *   * @size: size of the triangle
+ *    */
 void print_triangle(int size)
 {
-int sp, ro, tr;
-if (size <= 0)
-{
-_putchar("\n");
-}
-else
-{
-for (ro = 0; ro <= (size = 1); ro++)
-{
-for (sp = 0; sp < (size - 1) - ro; sp++)
-{
-_putchar(' ');
-}
-for (tr = 0; tr <= ro; tr++)
-{
-_putchar('#')
-}
-_putchar('\n');
-}
-}
+		if (size <= 0)
+				{
+							_putchar('\n');
+								} else
+										{
+													int i, j;
+
+															for (i = 1; i <= size; i++)
+																		{
+																						for (j = i; j < size; j++)
+																										{
+																															_putchar(' ');
+																																		}
+
+																									for (j = 1; j <= i; j++)
+																													{
+																																		_putchar('#');
+																																					}
+
+																												_putchar('\n');
+																														}
+																}
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,15 @@
  */
 void more_numbers(void)
 {
-	int x, y;
+	int i, j;
 
-	for (x = 0; x < 10; x++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (y = 0; y < 15; y++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (y >= 10)
-				_putchar((y / 10) + '0')
-			_putchar((y % 10) + '0');
+			if (j >= 10)
+				_putchar('1')
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
