c87ee71e2c8e14e04d6d0bae80923d104d04d7dd
ZubbsZubbs <thankgodazubuike194@gmail.com>
2023-02-26 04:10:27 -0800

    5-m

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * more_numbers - printing more numbers
+ * more_numbers -> printing more numbers
  */
 void more_numbers(void)
 {
-	int x, y;
+		int x, y;
 
-	for (x = 0; x <= 10; x++)
-	{
-	for (y = 0; y <= 14; y++)
-	{
-	if (y > 9)
-	{
-	_putchar((y / 10) + '0');
-	}
-	_putchar((y % 10) + '0');
-	}
-	_putchar('\n');
-	}
+		for (x = 0; x < 10; x++)
+		{
+		for (y = 0; y < 15; y++)
+		{
+		if (y >= 10)
+		_putchar((y / 10) + '0');
+		_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+											}
 }
