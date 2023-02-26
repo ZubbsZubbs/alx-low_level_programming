1f8341b1a383f2583676f08b6643a34988a23f97
ZubbsZubbs <thankgodazubuike194@gmail.com>
2023-02-26 03:18:52 -0800

    100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 		long x, maxf;
-		long number = 612852475143
+		long number = 612852475143;
 		double square = sqrt(number);
 
 		for (x = 1; x <= square; x++)
 		{
 		if (number % x == 0)
-		}
-		maxf = number / x
+		{
+		maxf = number / x;
 		}
 		}
 		printf("%ld\n", maxf);
