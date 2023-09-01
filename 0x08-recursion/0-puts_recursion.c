1d30200167e037bb5551e3919e5cf01c4251aa77
ZubbsZubbs <thankgodazubuike194@gmail.com>
2023-09-01 22:29:58 +0000

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void _puts_recursion(char *s);
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		 _putchar('\n');
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Ongoing recursion project
