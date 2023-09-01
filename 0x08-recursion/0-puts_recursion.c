2a8cfe51fb4e79c3719d41dece0cc9ca7a25b082
ZubbsZubbs <thankgodazubuike194@gmail.com>
2023-09-01 22:47:54 +0000

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ *
+ * Return: void
+ */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
