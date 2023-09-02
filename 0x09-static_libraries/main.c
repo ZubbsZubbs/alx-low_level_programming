527b39b2015b0ad37e5f8aa9d432ad76d736355e
ZubbsZubbs <thankgodazubuike194@gmail.com>
2023-09-02 06:26:56 +0000

    commit

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,18 @@
 #include "main.h"
 
+/**
+* main - the entry point of a program
+*
+* _puts - a function that prints a string followed by a new line
+*
+* @strings to print
+*
+* Return: integer
+*/
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n
+\t- Kevin Mitnick");
+return (0);
 }
